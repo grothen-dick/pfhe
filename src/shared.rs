2ae775103a6602f006b8c5a815743e68b9c78829
Oscar Roche <oscar.w.roche@gmail.com>
2023-09-27 17:05:18 +0200

    :fire: remove unneeded shared stuff

--- src/shared.rs
+++ src/shared.rs
@@ -1,26 +1,9 @@
 use crypto_bigint::U256;
 
-/// default size of a BigInt (in LIMBS)
+/// Default size of a BigInt (in LIMBS)
 pub const DEFAULT_LIMBS: usize = U256::LIMBS;
 
-/// an enum to specify wether to use Wrapping of Checked arithmetics
-pub enum Arithmetics {
-    Wrapping,
-    Checked,
-}
-
-/// the type of arithmetics used globally
-pub const ARITHMETICS: Arithmetics = Arithmetics::Wrapping;
-
-/// a macro to create a new BigInt with arithmetics specified by ARITHMETICS
-#[macro_export]
-macro_rules! new_usable_bigint {
-    ($expression: expr, $arithmetics: path) => {
-        $arithmetics($expression)
-    };
-}
-
-/// A trait to retrieve the current size of a const-generic struct
+/// Trait to retrieve the current size of a const-generic struct
 pub trait Bounded {
     const L: usize;
 
