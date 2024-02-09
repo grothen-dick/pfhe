bf54df379d0968e96c3fc23db6c1ae708e38fe65
Juul-Mc-Goa <jules.magois.pro@proton.me>
2024-02-09 17:05:08 +0100

    fix benches method call to `from_params`

--- benches/default.rs
+++ benches/default.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use pfhe::{
     bigint::{BigIntTrait, WrappingCryptoBigInt},
-    crypto_parameters::PrivateKeySchemeCryptographicParameters,
+    crypto_parameters::{EncryptionScheme, PrivateKeySchemeCryptographicParameters},
     hensel_code::HenselCode,
     rational::Rational,
 };
@@ -25,7 +25,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     println!("generating crypto params ...");
     let now = Instant::now();
-    let crypto_params = PrivateKeySchemeCryptographicParameters::<T1>::from_params(8, 3);
+    let crypto_params = PrivateKeySchemeCryptographicParameters::<T1>::new_from_params(8, 3);
     println!("crypto params generated in {:.2?}", now.elapsed());
 
     let message: Rational<T1> = Rational {
